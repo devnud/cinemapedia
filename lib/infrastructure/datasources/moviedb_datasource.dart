f68e7ac43bbf252242ad3de1ce11911b158c62b1
devnud <taximadridavid@gmail.com>
2023-12-27 20:02:05 +0100

    movieRepositoryImpl creado

--- lib/infrastructure/datasources/moviedb_datasource.dart
+++ lib/infrastructure/datasources/moviedb_datasource.dart
@@ -1,6 +1,8 @@
+import 'package:cinemapedia/infrastructure/mappers/movie_mapper.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/moviedb_response.dart';
 import 'package:dio/dio.dart';
 import 'package:cinemapedia/config/constants/environment.dart';
-import 'package:cinemapedia/domain/datasources/moies_datasources.dart';
+import 'package:cinemapedia/domain/datasources/movies_datasources.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 
 class MoviedbDatasources extends MoviesDataSource {
@@ -15,9 +17,14 @@ class MoviedbDatasources extends MoviesDataSource {
   );
   @override
   Future<List<Movie>> getNowPlaying({int page = 1}) async {
-
     final response = await dio.get('/movie/now_playing');
-    final List<Movie> movies = [];
+
+    final movieDbResponse = MovieDbResponse.fromJson(response.data);
+
+    final List<Movie> movies = movieDbResponse.results
+        .where((moviedb) => moviedb.posterPath != 'no-poster')
+        .map((moviedb) => MovieMapper.movieDBToEntity(moviedb))
+        .toList();
 
     return movies;
   }
