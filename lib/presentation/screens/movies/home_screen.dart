937f5553324b8328b95adf32e15984b4c1d26b25
devnud <taximadridavid@gmail.com>
2023-12-30 14:44:09 +0100

    Movie Horizontal ListView empenzando

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -33,7 +33,7 @@ class _HomeViewState extends ConsumerState<_HomeView> {
 
   @override
   Widget build(BuildContext context) {
-    // final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
+    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
 
     final slideshowMovies = ref.watch(moviesSlideshowProvider);
 
@@ -41,6 +41,11 @@ class _HomeViewState extends ConsumerState<_HomeView> {
       children: [
         const CustomAppbar(),
         MoviesSlideshow(movies: slideshowMovies),
+        MovieHorizontalListview(
+          movies: nowPlayingMovies,
+          title: 'En Cines',
+          subtitle: 'Lunes 20',
+        ),
       ],
     );
   }
--- lib/presentation/widgets/widgets.dart
+++ lib/presentation/widgets/widgets.dart
@@ -1,3 +1,6 @@
 export 'package:cinemapedia/presentation/widgets/shared/custom_appbar.dart';
 export 'package:cinemapedia/presentation/widgets/movies/movies_slideshow.dart';
 export 'package:cinemapedia/presentation/widgets/movies/custom_bottom_navigation.dart';
+export 'package:cinemapedia/presentation/widgets/movies/movie_horizontal_listview.dart';
+
+
